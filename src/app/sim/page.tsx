ca68137cf2c3e045c847854a6326203c6ed98723
eachen1010 <eachen1010@gmail.com>
2025-02-05 17:03:14 -0800

    Committing formatting changes to vehicle and sim pages

--- src/app/sim/page.tsx
+++ src/app/sim/page.tsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import VehicleSelectSidebar from '../components/VehicleSelectSidebar/VehicleSelectSidebar'
+'use client';
+import React, { useState } from 'react'
+import VehicleSelectSidebar from '../components/SimControlPanel/SimControlPanel'
 import DataSelectSidebar from '../components/DataSelectSidebar/DataSelectSidebar'
 import Navbar from '../components/Navbar/Navbar'
 
 const SimPage = () => {
+  const [selectedVehicle, setSelectedVehicle] = useState<string | number | null>(null);
+  const handleVehicleSelect = (vehicle_id: number) => {
+    setSelectedVehicle(vehicle_id);
+  };
+
   return (
     <>
-        <div className='flex h-screen flex-row font-mono' data-theme="luxury">
-            <DataSelectSidebar/>
-            <div className='flex-grow w-full h-full flex items-center justify-center rounded-3xl bg-gray-500'
+        <div className='fixed right-0 flex h-screen w-3/4 flex-row font-mono' data-theme="luxury">
+            {/* <DataSelectSidebar/> */}
+            <div className='w-full h-full flex items-center justify-center rounded-3xl bg-gray-500'
             style={{backgroundColor: '#110e12'}}>
-                <p>Select a Vehicle</p>
+                <p>Configure control panel</p>
             </div>
-            <VehicleSelectSidebar/>
+            <VehicleSelectSidebar onSelect={handleVehicleSelect}/>
+            <Navbar/>
         </div>
-        <Navbar/>  
+        
     </>
   )
 }
--- src/app/vehicles/page.tsx
+++ src/app/vehicles/page.tsx
@@ -1,115 +1,86 @@
 'use client';
-import React, { useState } from 'react'
-import VehicleEditSidebar from '../components/VehicleEditSidebar/VehicleEditSidebar'
+import React, { useState, useEffect } from 'react'
+import VehicleEditSidebar from '../components/VehicleEditComponents/VehicleEditSidebar/VehicleEditSidebar'
 import Navbar from '../components/Navbar/Navbar'
 import CSVImporterBox from '../components/CSVImporterBox/CSVImporterBox';
 
-const VehiclesPage = () => {
-  const [selectedVehicle, setSelectedVehicle] = useState<string | null>(null);
+interface VehicleData {
+  vehicle_name: string;
+  vehicle_type: string;
+  total_mass: number;
+}
 
-  const handleVehicleSelect = (vehicle: string) => {
-    setSelectedVehicle(vehicle);
+const VehiclesPage: React.FC<VehicleData> = () => {
+  const [selectedVehicle, setSelectedVehicle] = useState<string | number | null>(null);
+
+  const [vehicleData, setVehicleData] = useState<VehicleData | null>(null);
+
+  const handleVehicleSelect = (vehicle_id: number) => {
+    setSelectedVehicle(vehicle_id);
   };
+
+  const getVehicleData = async (vehicle_id: number) => {
+    const res = await fetch(`http://localhost:3000/api/get_vehicle?vehicle_id={vehicle_id}`.replace('{vehicle_id}', vehicle_id.toString()), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+    setVehicleData(data);
+    console.log(vehicleData);
+    return data;
+  }
+
+  useEffect(() => {
+    if (typeof selectedVehicle === 'number') {
+      getVehicleData(selectedVehicle).then((data) => {
+        console.log(data);
+      });
+    }
+    }, [selectedVehicle]);
 
   return (
     <>
-      <Navbar />
-      <div className="flex h-screen flex-row font-mono" data-theme="luxury">
+      <div className="fixed right-0 flex h-screen w-3/4 flex-row font-mono" data-theme="luxury">
+        <Navbar />
         <div
           className={`flex-grow w-full h-full flex items-center justify-center rounded-3xl`}
           style={{ backgroundColor: '#110e12' }}
         >
             {selectedVehicle === '+' ? (
-            <div className="flex flex-col items-center">
-              <p>Add a new vehicle</p>
-              <CSVImporterBox />
-              {/* Add your form or content for adding a new vehicle here */}
-            </div>
-            ) : selectedVehicle ? (
-            <div className="flex flex-col items-center">
-              <p>Selected Vehicle: {selectedVehicle}</p>
-              <div className="join">
-              <input
-                className="join-item btn btn-square w-auto px-4"
-                type="radio"
-                name="options"
-                aria-label="General"
-                defaultChecked
-              />
-              <input
-                className="join-item btn btn-square w-auto px-4"
-                type="radio"
-                name="options"
-                aria-label="Inertia"
-              />
-              <input
-                className="join-item btn btn-square w-auto px-4"
-                type="radio"
-                name="options"
-                aria-label="Dimensions"
-              />
-              <input
-                className="join-item btn btn-square w-auto px-4"
-                type="radio"
-                name="options"
-                aria-label="Steering"
-              />
-              <input
-                className="join-item btn btn-square w-auto px-4"
-                type="radio"
-                name="options"
-                aria-label="Aerodynamics"
-              />
-              <input
-                className="join-item btn btn-square w-auto px-4"
-                type="radio"
-                name="options"
-                aria-label="Brakes"
-              />
-              <input
-                className="join-item btn btn-square w-auto px-4"
-                type="radio"
-                name="options"
-                aria-label="Tyres"
-              />
-              <input
-                className="join-item btn btn-square w-auto px-4"
-                type="radio"
-                name="options"
-                aria-label="Engine"
-              />
-              <input
-                className="join-item btn btn-square w-auto px-4"
-                type="radio"
-                name="options"
-                aria-label="Transmission"
-              />
+              <div className="flex flex-col items-center">
+                <p>Add a new vehicle</p>
+                <CSVImporterBox />
               </div>
-              <table className="table-auto mt-4">
-              <thead>
-                <tr>
-                <th className="px-4 py-2">Attribute</th>
-                <th className="px-4 py-2">Value</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                <td className="border px-4 py-2">Attribute 1</td>
-                <td className="border px-4 py-2">Value 1</td>
-                </tr>
-                <tr>
-                <td className="border px-4 py-2">Attribute 2</td>
-                <td className="border px-4 py-2">Value 2</td>
-                </tr>
-                <tr>
-                <td className="border px-4 py-2">Attribute 3</td>
-                <td className="border px-4 py-2">Value 3</td>
-                </tr>
-              </tbody>
-              </table>
-            </div>
+            ) : selectedVehicle ? (
+              <div className="flex flex-col items-center">
+                <p>Selected Vehicle: {vehicleData?.vehicle_name}</p>
+                <table className="table-auto mt-4">
+                <thead>
+                  <tr>
+                  <th className="px-4 py-2">Attribute</th>
+                  <th className="px-4 py-2">Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                  <td className="border px-4 py-2">Name</td>
+                  <td className="border px-4 py-2">{vehicleData?.vehicle_name}</td>
+                  </tr>
+                  <tr>
+                  <td className="border px-4 py-2">Type</td>
+                  <td className="border px-4 py-2">{vehicleData?.vehicle_type}</td>
+                  </tr>
+                  <tr>
+                  <td className="border px-4 py-2">Total Mass</td>
+                  <td className="border px-4 py-2">{vehicleData?.total_mass}</td>
+                  </tr>
+                </tbody>
+                </table>
+              </div>
             ) : (
-            <p>Select a Vehicle</p>
+              <p>Select a Vehicle</p>
             )}
         </div>
         <VehicleEditSidebar onSelect={handleVehicleSelect} />
