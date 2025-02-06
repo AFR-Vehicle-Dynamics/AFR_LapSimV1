e31fe6da1bdc887ec21e7adce27c500c73c2c6c1
Emmy <EmmyChen@dhcp-v018-207.mobile.uci.edu>
2025-02-05 16:05:40 -0800

    Committing convert vehicle select sidebar to controlpanel

---
+++ src/app/components/SimControlPanel/SimControlPanel.tsx
@@ -0,0 +1,92 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import VehicleModal from '../VehicleModal/VehicleModal';
+
+interface SimControlPanelProps {
+  onSelect: (vehicle_id: number) => void;
+}
+
+const openModal = (id: string) => {
+    const modal = document.getElementById(id) as HTMLDialogElement;
+    console.log("clicked me");
+    modal?.showModal();
+};
+
+const SimControlPanel: React.FC<SimControlPanelProps> = ({ onSelect }) => {
+  const [vehicles, setVehicles] = useState<{ id: number, vehicle_name: string }[]>([]);
+
+  const handleSelect = (vehicle_id: number) => {
+    onSelect(vehicle_id);
+  };
+
+  const getAllVehicleData = async () => {
+    const res = await fetch('http://localhost:3000/api/get_all_vehicles', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
+    setVehicles(data);
+    console.log(vehicles);
+    return data;
+  };
+
+  useEffect(() => {
+    getAllVehicleData().then((data) => {
+      console.log(data);
+    });
+  }, []);
+
+  return (
+    <div className="flex" data-theme="luxury">
+      {/* Sidebar */}
+      <div
+        // state to control width and visibility
+        className={`fixed h-[calc(100vh-2rem)] transition-all duration-300 z-10 mr-3 ml-32 mt-3 mb-3 w-auto left-0 font-mono pt-5 bg-[#20171e] rounded-xl`}>
+        {/* Sidebar content */}
+
+        <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center">
+                <div className="mt-4">
+                <h1>CONTROL PANEL</h1>
+            </div>
+            {vehicles.map((vehicle, index) => (
+            <div key={index} className="mt-4 text-center">
+              <button
+                className="btn font-mono btn-outline hover:text-blue-500 w-64"
+                onClick={() => handleSelect(vehicle.id)}
+              >
+                {vehicle.vehicle_name}
+              </button>
+            </div>
+          ))}
+        </div>
+        
+        {/* <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center">
+                <div className="mt-4">
+                <h1>VEHICLE SELECT</h1>
+            </div>
+        </div>
+        <div className="mt-4 text-center">
+            <VehicleModal id='JINX'/>
+          </div>
+          <div className="mt-4 text-center">
+            <VehicleModal id='GASTLY'/>
+          </div>
+          <div className="mt-4 text-center">
+            <VehicleModal id='+'/>
+          </div>
+        </div>
+      </div> */}
+      </div>
+      {/* Main content */}
+      <div className={`flex-1 p-4 ml-64`}>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default SimControlPanel
