e910944fcfc1d36b5c55be2809b0fe2239a25d94
eachen1010 <eachen1010@gmail.com>
2025-02-07 12:40:36 -0800

    Updated csv importer and navbar

--- src/app/components/CSVImporterBox/CSVImporterBox.tsx
+++ src/app/components/CSVImporterBox/CSVImporterBox.tsx
@@ -1,56 +1,90 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import Papa from 'papaparse';
 
 const acceptableCSVFileTypes = '.csv';
 
 
 const CSVImporterBox = () => {
+    const [error, setError] = useState(false);
 
     const onFileChangeHandler = (event: any) => {
         console.log(event.target.files[0]);
         const csvFile = event.target.files[0];
         let result;
+        if (csvFile.type !== 'text/csv') {
+            // console.error("Invalid file format. Please upload a CSV file.");
+            setError(true);
+            return;
+        } else {
+            setError(false);
+        }
+
         Papa.parse<File>(csvFile, {
             skipEmptyLines: true,
             complete: function(results: Papa.ParseResult<any>) {
-                console.log("Finished!", results.data);
-                // let lastFilledFirstElement: string | null = null;
-                // const jsonData = results.data.map((row: any, index: number) => {
-                //     if (!row[0] && lastFilledFirstElement !== null) {
-                //         row[0] = lastFilledFirstElement;
-                //     } else {
-                //         lastFilledFirstElement = row[0];
-                //     }
-                //     return {
-                //         id: index,
-                //         column1: row[0],
-                //         column2: row[1],
-                //         column3: row[2],
-                //     };
-                // });
-                result = addVehicle(results.data);
+            console.log("Finished!", results.data);
+            result = addVehicle(results.data);
+            },
+            error: function(error) {
+            console.error("Error parsing CSV file:", error);
             }
         });
     }
 
-    useEffect(() => {
-        fetch("http://localhost:3000/api/hello").then(
-            response => response.json()
-        ).then((data) => {
-            console.log(data);
-        });
-    }, []);
-
     const addVehicle = async (data: any[]) => {
-        const res = await fetch('/api/route-name', {
+        const res = await fetch('http://localhost:3000/api/add_vehicle', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                "vehicle_name": data[1][2],
-                "vehicle_type": data[2][2],
-                "total_mass": parseFloat(data[3][2])
+                "vehicle_name": data[1][2] ?? '',
+                "vehicle_type": data[2][2] ?? '',
+                "total_mass": parseFloat(data[3][2] ?? '0'),
+                "fm_distribution": parseFloat(data[4][2] ?? '0'),
+                "wheelbase": parseInt(data[5][2] ?? '0'),
+                "steering_rack_ratio": parseFloat(data[6][2] ?? '0'),
+                "lift_coef": parseFloat(data[7][2] ?? '0'),
+                "drag_coef": parseFloat(data[8][2] ?? '0'),
+                "cl_scale_mult": parseFloat(data[9][2] ?? '0'),
+                "cd_scale_mult": parseFloat(data[10][2] ?? '0'),
+                "fa_distribution": parseFloat(data[11][2] ?? '0'),
+                "frontal_area": parseFloat(data[12][2] ?? '0'),
+                "air_density": parseFloat(data[13][2] ?? '0'),
+                "disc_outer_dia": parseFloat(data[14][2] ?? '0'),
+                "pad_height": parseFloat(data[15][2] ?? '0'),
+                "pad_f_coef": parseFloat(data[16][2] ?? '0'),
+                "caliper_num_pistons": parseInt(data[17][2] ?? '0'),
+                "caliper_piston_dia": parseFloat(data[18][2] ?? '0'),
+                "master_cylinder_dia": parseFloat(data[19][2] ?? '0'),
+                "pedal_ratio": parseFloat(data[20][2] ?? '0'),
+                "grip_factor_mult": parseFloat(data[21][2] ?? '0'),
+                "tyre_r": parseFloat(data[22][2] ?? '0'),
+                "rolling_resistance": parseFloat(data[23][2] ?? '0'),
+                "long_friction_coef": parseFloat(data[24][2] ?? '0'),
+                "long_friction_load_rating": parseFloat(data[25][2] ?? '0'),
+                "long_friction_sensitivity": parseFloat(data[26][2] ?? '0'),
+                "lat_friction_coef": parseFloat(data[27][2] ?? '0'),
+                "lat_friction_load_rating": parseFloat(data[28][2] ?? '0'),
+                "lat_friction_sensitivity": parseFloat(data[29][2] ?? '0'),
+                "front_cornering_stiffness": parseFloat(data[30][2] ?? '0'),
+                "rear_cornering_stiffness": parseFloat(data[31][2] ?? '0'),
+                "power_factor_mult": parseFloat(data[32][2] ?? '0'),
+                "thermal_eff": parseFloat(data[33][2] ?? '0'),
+                "fuel_lower_heating_value": parseFloat(data[34][2] ?? '0'),
+                "drive_type": data[35][2] ?? '',
+                "gear_shift_time": parseFloat(data[36][2] ?? '0'),
+                "primary_gear_eff": parseFloat(data[37][2] ?? '0'),
+                "final_gear_eff": parseFloat(data[38][2] ?? '0'),
+                "gearbox_eff": parseFloat(data[39][2] ?? '0'),
+                "primary_gear_reduc": parseFloat(data[40][2] ?? '0'),
+                "final_gear_reduc": parseFloat(data[41][2] ?? '0'),
+                "first_gear_ratio": parseFloat(data[42][2] ?? '0'),
+                "second_gear_ratio": parseFloat(data[43][2] ?? '0'),
+                "third_gear_ratio": parseFloat(data[44][2] ?? '0'),
+                "fourth_gear_ratio": parseFloat(data[45][2] ?? '0'),
+                "fifth_gear_ratio": parseFloat(data[46][2] ?? '0'),
+                "sixth_gear_ratio": parseFloat(data[47][2] ?? '0')
             }),
         })
 
@@ -66,6 +100,8 @@ const CSVImporterBox = () => {
             onChange={onFileChangeHandler}
             className="border border-gray-300 p-2 rounded"
             />
+            {/* Make a button */}
+            {error && <p className="text-red-500 mt-2">Invalid file format. Please upload a CSV file.</p>}
         </div>
 
     )
--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -3,15 +3,17 @@ import Link from 'next/link';
 import { GiRaceCar } from "react-icons/gi";
 import { MdMenu } from "react-icons/md";
 import { IoMdHome, IoIosSpeedometer } from "react-icons/io";
+import { FaRoad } from "react-icons/fa";
 import Image from 'next/image';
 import AFRLogo from '../../assets/afrlogo.png';
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 h-full z-20 flex flex-col items-center gap-5 bg-transparent w-1/12" data-theme="luxury">
-      <ul className="flex flex-col items-center gap-5 p-5">
-      <li className="tooltip tooltip-right overflow-visible pb-10" data-tip="Home"><Link href="/"><Image src={AFRLogo} alt="AFR Logo" width={38} height={38} className="w-8 h-8"/></Link></li>
+    <div className="fixed top-0 left-0 h-full z-20 flex flex-col items-center bg-transparent w-1/12 mt-6" data-theme="luxury">
+      <ul className="flex flex-col items-center gap-9 p-5">
+      <li className="tooltip tooltip-right overflow-visible pb-5" data-tip="Home"><Link href="/"><Image src={AFRLogo} alt="AFR Logo" width={38} height={38} className="w-8 h-8"/></Link></li>
       <li className="tooltip tooltip-right overflow-visible" data-tip="Vehicles"><Link href="/vehicles"><GiRaceCar className="w-9 h-9"/></Link></li>
+      <li className="tooltip tooltip-right overflow-visible" data-tip="Tracks"><Link href="/tracks"><FaRoad className="w-7 h-7"/></Link></li>
       <li className="tooltip tooltip-right overflow-visible" data-tip="Simulator"><Link href="/sim"><IoIosSpeedometer className="w-8 h-8"/></Link></li>
       </ul>
     </div>
