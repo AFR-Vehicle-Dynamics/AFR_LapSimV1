ce7b3a1ad0a0be46c21d7ee72a5fccc036683df8
eachen1010 <eachen1010@gmail.com>
2025-02-19 16:46:48 -0800

    Deleting a few files

--- api/vehicle_model.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request, flash
-
-class VehicleModel(db.Model):
-    __tablename__ = 'simple_vehicle'
-    id = db.Column(db.Integer, primary_key=True)
-    vehicle_name = db.Column(db.String)
-    vehicle_type = db.Column(db.String)
-    total_mass = db.Column(db.Float)
